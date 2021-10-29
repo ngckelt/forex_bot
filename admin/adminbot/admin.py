528d1d57306689205b10f35202226b5928ca28f7
nick <kamarinskiv@mail.ru>
2021-10-29 17:59:41 +0700

    Скрыл пункты в админке

--- admin/adminbot/admin.py
+++ admin/adminbot/admin.py
@@ -19,41 +19,41 @@ class BotAdminsAdmin(admin.ModelAdmin):
     class Meta:
         model = models.BotAdmins
 
-
-@admin.register(models.Deposits)
-class DepositsAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime', 'amount', 'card_number']
-    search_fields = ['first_name', 'last_name', 'username', 'telegram_id']
-
-    class Meta:
-        model = models.Deposits
-
-
-@admin.register(models.Withdrawals)
-class WithdrawalsAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime', 'amount', 'card_number']
-    search_fields = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime']
-
-    class Meta:
-        model = models.Withdrawals
-
-
-@admin.register(models.ReferralAccruals)
-class ReferralAccrualsAdmin(admin.ModelAdmin):
-    list_display = ['amount', 'accrual_to', 'accrual_from', 'datetime']
-    search_fields = ['accrual_to', 'accrual_from']
-
-    class Meta:
-        model = models.ReferralAccruals
-
-
-@admin.register(models.MonthlyAccruals)
-class MonthlyAccrualsAdmin(admin.ModelAdmin):
-    list_display = ['client', 'amount', 'datetime']
-    search_fields = ['client']
-
-    class Meta:
-        model = models.MonthlyAccruals
+#
+# @admin.register(models.Deposits)
+# class DepositsAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime', 'amount', 'card_number']
+#     search_fields = ['first_name', 'last_name', 'username', 'telegram_id']
+#
+#     class Meta:
+#         model = models.Deposits
+#
+#
+# @admin.register(models.Withdrawals)
+# class WithdrawalsAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime', 'amount', 'card_number']
+#     search_fields = ['first_name', 'last_name', 'username', 'telegram_id', 'datetime']
+#
+#     class Meta:
+#         model = models.Withdrawals
+#
+#
+# @admin.register(models.ReferralAccruals)
+# class ReferralAccrualsAdmin(admin.ModelAdmin):
+#     list_display = ['amount', 'accrual_to', 'accrual_from', 'datetime']
+#     search_fields = ['accrual_to', 'accrual_from']
+#
+#     class Meta:
+#         model = models.ReferralAccruals
+#
+#
+# @admin.register(models.MonthlyAccruals)
+# class MonthlyAccrualsAdmin(admin.ModelAdmin):
+#     list_display = ['client', 'amount', 'datetime']
+#     search_fields = ['client']
+#
+#     class Meta:
+#         model = models.MonthlyAccruals
 
 
 @admin.register(models.CardDetails)
